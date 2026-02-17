7ada7eb4b5d3957ba1fc5a0b261727fe44ab9a70
chaitanya sahu <csahu4064@gmail.com>
2026-02-17 22:01:50 +0530

    Create add_two_numbers.py

---
+++ python-functions-assignment/add_two_numbers.py
@@ -0,0 +1,11 @@
+def add_numbers(num1, num2):
+    return num1 + num2
+
+
+
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+result = add_numbers(number1, number2)
+
+print("Sum is:", result)
