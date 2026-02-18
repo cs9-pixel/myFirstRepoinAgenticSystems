552f2f7bd3c0f192a1c100c7b9dd44f676770b00
chaitanya sahu <csahu4064@gmail.com>
2026-02-18 22:46:01 +0530

    Create student_marks_analyzer.py

---
+++ python-data-assignment/student_marks_analyzer.py
@@ -0,0 +1,10 @@
+
+marks = [78, 85, 90, 65, 88, 92, 80, 75]
+
+print("Full list:", marks)
+print("First 3 marks:", marks[:3])
+print("Last 3 marks:", marks[-3:])
+
+print("Highest:", max(marks))
+print("Lowest:", min(marks))
+print("Average:", sum(marks) / len(marks))
